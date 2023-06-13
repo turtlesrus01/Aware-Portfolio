2f15614afe5dc1a93afdba3ba74c80bce390c153
Green Camper <stefan.palacios@gmail.com>
2023-06-12 23:02:48 -0500

    Added About me page

---
+++ src/component/pages/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac ipsum
+        in nisl venenatis fermentum. Aenean a posuere augue. Curabitur quis
+        tristique arcu, vel dictum arcu. Curabitur pretium ornare purus, ut
+        ullamcorper nisi. Nullam sit amet elementum sapien. Fusce eu magna et
+        justo pharetra vulputate. Nunc libero nibh, pretium in feugiat at,
+        elementum vel odio. In porta diam a velit gravida iaculis. Phasellus
+        vestibulum nulla vel fringilla laoreet. Quisque et sem leo. Vivamus
+        malesuada libero a lacinia porttitor. Morbi accumsan mollis odio
+        pharetra ultrices. Sed quis mi pellentesque, cursus odio ac, gravida
+        sapien. Phasellus posuere fringilla libero vitae finibus. Ut aliquam
+        gravida magna vel auctor. Pellentesque posuere libero eget sollicitudin
+        ultricies.
+      </p>
+    </div>
+  );
+}
