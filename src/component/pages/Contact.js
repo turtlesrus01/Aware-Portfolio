28f6038749a762688e9bd64033132b3b2161a4c4
Green Camper <stefan.palacios@gmail.com>
2023-06-15 18:37:37 -0500

    Added column styling to contact page

--- src/component/pages/Contact.js
+++ src/component/pages/Contact.js
@@ -3,11 +3,11 @@ import { validateEmail } from "../utils/helpers";
 
 export default function Contact() {
   //state variable setup
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [status, setStatus] = useState('Submit');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [status, setStatus] = useState("Submit");
 
   //handInputChange event arrow function
   const handleInputChange = (e) => {
@@ -17,12 +17,12 @@ export default function Contact() {
     const inputValue = target.value;
 
     //state setter if statement
-    if (inputType === 'name') {
+    if (inputType === "name") {
       setName(inputValue);
-    } else if (inputType === 'email') {
+    } else if (inputType === "email") {
       setEmail(inputValue);
     } else {
-      setMessage(inputValue)
+      setMessage(inputValue);
     }
   };
 
@@ -33,7 +33,7 @@ export default function Contact() {
     //validate email if statement incase an invalid email is entered
     if (!validateEmail(email)) {
       //Error message for invalid input
-      return setErrorMessage('Email or name is invalid');
+      return setErrorMessage("Email or name is invalid");
     }
 
     //Arrange input data into JSON format
@@ -42,71 +42,82 @@ export default function Contact() {
       name: name.value,
       email: email.value,
       message: message.value,
-    }
+    };
     console.log(data);
 
     //fetch POST to localhost (email send)
-    let response = await fetch('http://localhost:5000/contact', {
-      method: 'POST',
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     });
-    setStatus('Submit');
+    setStatus("Submit");
     //results
     const result = await response.json();
     console.log(result);
 
     //Clear fields after post
-    setName('');
-    setEmail('');
-    setMessage('');
-  }
-    
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
 
   return (
     <div>
       <h1>Contact Me</h1>
-      {/* form */}
-      <form className="form" onSubmit={handleFormSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input 
-          value={name} 
-          type='text' 
-          id="name"
-          onChange={handleInputChange}
-          placeholder="name" 
-          required />
+      <div className="container">
+        <div className="row">
+          <div className="col-sm m-5"></div>
+          <div className="col-sm m-5">
+            {/* form */}
+            <form className="form" onSubmit={handleFormSubmit}>
+              <div>
+                <label htmlFor="name">Name:</label>
+                <input
+                  value={name}
+                  type="text"
+                  id="name"
+                  onChange={handleInputChange}
+                  placeholder="name"
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                  value={email}
+                  type="email"
+                  id="email"
+                  onChange={handleInputChange}
+                  placeholder="email"
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="message">Message:</label>
+                <input
+                  value={message}
+                  type="text"
+                  id="message"
+                  onChange={handleInputChange}
+                  placeholder="message"
+                  required
+                />
+              </div>
+              <button type="submit" onClick={handleFormSubmit}>
+                {status}
+              </button>
+            </form>
+            {errorMessage && (
+              <div>
+                <p className="error-text">{errorMessage}</p>
+              </div>
+            )}
+          </div>
         </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input 
-          value={email} 
-          type='email' 
-          id="email"
-          onChange={handleInputChange}
-          placeholder="email"
-          required />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <input 
-          value={message} 
-          type='text' 
-          id="message"
-          onChange={handleInputChange}
-          placeholder="message" 
-          required />
-        </div>
-        <button type='submit' onClick={handleFormSubmit}>{status}</button>
-      </form>
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage}</p>
-        </div>
-      )}
+      </div>
     </div>
   );
 }
