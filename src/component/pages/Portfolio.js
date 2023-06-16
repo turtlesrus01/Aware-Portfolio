f416a419251c51d9b5d5f52fbc44d7d274de246f
Green Camper <stefan.palacios@gmail.com>
2023-06-16 00:58:25 -0500

    Added an item to portfolio

--- src/component/pages/Portfolio.js
+++ src/component/pages/Portfolio.js
@@ -1,9 +1,17 @@
 import React from "react";
+import one from "../assets/img/pexels-photo-546819.webp";
 
 export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
+      <div className="container">
+        <div className="col-lg-4 col-md-6 portfolio-item ">
+          <a href="https://github.com/turtlesrus01/co-social">
+            <img src={one} alt="computer screen with code"></img>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
