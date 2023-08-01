a50da3f7a29a4e8e60cf0c1b48e175e32a0c34d0
Green Camper <stefan.palacios@gmail.com>
2023-07-31 23:19:11 -0500

    Description edit

--- src/component/pages/About.js
+++ src/component/pages/About.js
@@ -7,12 +7,18 @@ export default function About() {
       <section className="about">
         <div className="container aos-init">
           <h1>About Me</h1>
-          <img src={userLogo} alt="Logo" class="img-fluid m-5" />
-          <p>
-            My name is Stefan Palacios, I am an eager developer that wants to gain experience and knowledge with web development. 
-            I am just about finished working on a codind bootcamp that is helping me build my skills scripting, design, and database knowledge.
-            I feel comfortable most working with Javascript, HTML, and MongoDB for my applications and am always looking to learn about more technologies. 
-          </p>
+          <div className="aboutContainer">
+            <img src={userLogo} alt="Logo" className="img-fluid m-5" />
+            <p>
+              My name is Stefan Palacios, I am an eager developer that wants to
+              gain experience and knowledge with web development. I am just
+              about finished working on a codind bootcamp that is helping me
+              build my skills scripting, design, and database knowledge. I feel
+              comfortable most working with Javascript, HTML, and MongoDB for my
+              applications and am always looking to learn about more
+              technologies.
+            </p>
+          </div>
         </div>
       </section>
     </div>
